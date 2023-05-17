bd2d9568d244c332624b8c0cdc7797c8a510e4d8
prantikseal <83081442+prantikseal@users.noreply.github.com>
2023-05-17 22:10:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+return (
+    <>
+      <div className="container">
+          <div className="row">
+            <div className=" mt-5 pt-5 fw-bold col-md-6">
+              dsff
+              </div>
+              <div className="col-md-6 fw-bolder mt-5 pt-5">
+                lol
+              </div>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
